ce80d39a7bb3b576a49a45407a89b94d0cbe2096
guilleripa <guilleripa21@gmail.com>
2021-11-24 19:54:27 -0300

    Download dolar and use keyring.

--- itau.py
+++ itau.py
@@ -1,5 +1,8 @@
-import click
 import logging
+
+import click
+import keyring
+import requests
 
 from client import ItauClient
 
@@ -8,14 +11,23 @@ from client import ItauClient
 @click.option(
     "--username", help="Itau Link username (usually identity card from Uruguay)"
 )
-@click.option("--password", help="Itau Link weak password")
+@click.option(
+    "--password",
+    default=keyring.get_password("system", "pytau"),
+    help="Itau Link weak password",
+)
 @click.option(
     "--save-csv",
     is_flag=True,
-    help="Generate a CSV report for each account. (Saved in current directory)",
+    help="Generate a CSV report for each account. (Saved in `results` folder)",
+)
+@click.option(
+    "--csv-path",
+    default="results/",
+    help="Path to store csv results if enabled.",
 )
 @click.option("-v", "--verbose", count=True)
-def main(username, password, save_csv, verbose):
+def main(username, password, save_csv, csv_path, verbose):
     if verbose == 0:
         log_level = None
     elif verbose == 1:
@@ -33,12 +45,21 @@ def main(username, password, save_csv, verbose):
 
     client = ItauClient(username, password)
     if save_csv:
-        client.save()
+        client.save(path=csv_path)
     else:
         from IPython import embed
 
         embed(display_banner=False)
 
+    # download historic dolar values
+    historic_dolar = requests.get(
+        "http://www.ine.gub.uy/c/document_library/get_file?uuid=1dcbe20a-153b-4caf-84a7-7a030d109471"
+    )
+    with open("results/historic_dolar.xlsx", "wb") as file:
+        file.write(historic_dolar.content)
+
+
+# %%
 
 if __name__ == "__main__":
     main()
